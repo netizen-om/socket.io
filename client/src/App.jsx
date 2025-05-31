28a54fa8661c3dda9d585ea49f72148a138a1566
netizen-om <borisagarom@gmail.com>
2025-05-31 14:34:58 +0530

    added message sharing with rooms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { io } from "socket.io-client"
-
+import { Button, Container, TextField, Typography } from "@mui/material"
 import React from 'react'
-import Button from '../components/Button'
 
 function App() {
 
-  const socket = io("http://localhost:3000")
+  const socket = useMemo(() => io("http://localhost:3000"), [])
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Sending msg");
+    
+    socket.emit("message", {message, room})
+    setMessage("")
+  }
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
 
   useEffect(() => {
   
     socket.on("connect", () => {
-      
+      setSocketId(socket.id);
       console.log("Hello, Connected Id : ", socket.id);
 
       socket.on("welcome", (msg) => {
         console.log(msg);
-        
-        socket.emit("welcome", "Hey man")
+        // socket.emit("welcome", "Hey man")
       })
+
+      socket.on("recevied-msg", (revMsg) => {
+        console.log(revMsg);
+      })
+
     })
     
     return () => {
@@ -29,12 +44,24 @@ function App() {
   }, [])
 
 
-  return (
-    <div>
-      <div>App asdasdfqwfef</div>
-      <Button />
-    </div>
-  )
+  return <Container maxWidth='sm'> 
+
+    <Typography variant='h1' component={"div"} gutterBottom>
+      Welcome to Socket.io
+    </Typography>
+
+    <Typography variant='h5' component={"div"} gutterBottom>
+      {socketId}
+    </Typography>
+  
+    <form onSubmit={submitHandler}>
+      <TextField id='outlined-basic' value={message} onChange={(e) => setMessage(e.target.value)} label="Message" variant='outlined'></TextField>
+
+      <TextField id='outlined-basic' value={room} onChange={(e) => setRoom(e.target.value)} label="Room" variant='outlined'></TextField>
+      <Button type='submit' variant="contained" color="primary">Send</Button>
+    </form>
+
+  </Container>
 }
 
 export default App
--- server/app.js
+++ server/app.js
@@ -25,18 +25,25 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 
-    console.log("User connected");
+    console.log("User connected", socket.id);
     // console.log("Id", socket.id);
 
     // socket.broadcast.emit("welcome", "This is Broacasted msg")
 
-    socket.emit("welcome", "HZuheibwef")
+    // socket.emit("welcome", "HZuheibwef")
     socket.on("welcome", (msg) => {
         console.log(msg);
     })
 
     socket.on("disconnect", () => {
+        socket.broadcast.emit("welcome", `The user ${socket.id} disconnected`);
+        console.log(`The user ${socket.id} disconnected`);
         
+    })  
+
+    socket.on("message", (data) => {
+        console.log(data);
+        io.to(data.room).emit("recevied-msg", data.message);
     })
 
 })
